99a0e2077009df9fb6394a3343b60db59823a093
nikolayjuminov <nikolayjuminov@yandex.ru>
2025-03-28 11:08:35 +0400

    Решение задачи

--- First task/Program.cs
+++ First task/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Введите строку для поиска в ней символов x и w");
+		string? listOfSimbol = Console.ReadLine();
+		if (listOfSimbol != null && listOfSimbol != "")
+		{
+			Program.Cycle(listOfSimbol);
+		}
+		else
+			Console.WriteLine("-1");
+	}
+	private static void Cycle(string listOfSimbol)
+	{
+		for (int i = 0; i < listOfSimbol.Length; i++)
+		{
+			if ((string?)listOfSimbol.Substring(i, 1) == "x" || (string?)listOfSimbol.Substring(i, 1) == "X")
+			{
+				Console.WriteLine("Результат x");
+				break;
+			}
+			else if ((string?)listOfSimbol.Substring(i, 1) == "w" || (string?)listOfSimbol.Substring(i, 1) == "W")
+			{
+				Console.WriteLine("Результат w");
+				break;
+			}
+			else if (i == listOfSimbol.Length - 1)
+			{
+				Console.WriteLine("-1");
+			}
+
+		}
+	}
+}
+
